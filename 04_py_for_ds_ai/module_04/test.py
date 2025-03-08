baaf55caa7b59afb1ef512d4ab06668596c6ae5f
Yafas Eusha <myeusha29@isrt.ac.bd>
2025-03-08 19:42:35 +0600

    completed upto reading and writing files/ module 04/ py for ds ai/ ibm ds pro

---
+++ 04_py_for_ds_ai/module_04/test.py
@@ -0,0 +1,44 @@
+# with open("example2.txt", "w") as file:
+#     file.write("This is line A\n")
+#     file.write("This is line B\n")
+
+# with open("example2.txt", "r") as file:
+#     file_content = file.read()
+
+# print(file_content)
+
+
+
+# lines = ["This is line A", "This is line B", "This is line C"]
+
+# with open("example3.txt", "w") as file:
+#     for line in lines:
+#         file.write(line + "\n")
+
+# with open("example3.txt", "r") as file:
+#     file_content = file.read()
+
+# print(file_content)
+
+
+
+# new_line = "This is line D"
+
+# with open("example3.txt", "a") as file:
+#     file.write(new_line + "\n")
+
+
+# with open("example3.txt", "r") as file:
+#     file_content = file.read()
+
+# print(file_content)
+
+
+
+
+
+
+
+
+
+
